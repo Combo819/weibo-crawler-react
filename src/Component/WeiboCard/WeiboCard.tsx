c4cd69feb57d9ec3436a82db4e830a4016484f34
combo <617512696@qq.com>
2020-09-08 00:26:16 -0400

    add photo viewer

--- src/Component/WeiboCard/WeiboCard.tsx
+++ src/Component/WeiboCard/WeiboCard.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 import { Card, Avatar, Row, Col } from "antd";
 import { LikeOutlined, CommentOutlined } from "@ant-design/icons";
 import _ from "lodash";
 import HtmlParser from "react-html-parser";
 import ReactPlayer from "react-player";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 type CardProps = {
   weibo: any;
   children: React.ReactNode;
@@ -15,17 +17,32 @@ const getVideoUrl = (url: string): string => {
   return `http://localhost/videos/${fileName}`;
 };
 
+const getImageUrl = (url: string): string => {
+  const urlNoQuery = url.split("?")[0];
+  const fileName = urlNoQuery.split("/").pop();
+  return `http://localhost/images/${fileName}`;
+};
+
 export default function WeiboCard(props: CardProps) {
   const { weibo } = props;
-  const chunkImages: string[][] = _.chunk(weibo.pics);
+  const chunkImages: any[][] = _.chunk(weibo.pics, 3);
+
   return (
     <Card
       actions={[
         <div>
-          <CommentOutlined key="comment"></CommentOutlined>18
+          <CommentOutlined
+            style={{ position: "relative", top: -3 }}
+            key="comment"
+          ></CommentOutlined>
+          <span>{weibo.commentsCount}</span>
         </div>,
         <div>
-          <LikeOutlined key="like"></LikeOutlined>18
+          <LikeOutlined
+            style={{ position: "relative", top: -3 }}
+            key="like"
+          ></LikeOutlined>
+          <span> {weibo.attitudesCount}</span>
         </div>,
       ]}
       style={{ width: "100%" }}
@@ -43,8 +60,14 @@ export default function WeiboCard(props: CardProps) {
             {chunkImages.map((item) => (
               <Row gutter={[8, 8]} justify="start">
                 {item.map((ele) => (
-                  <Col key={ele} style={{ width: 150, overflow: "hidden" }}>
-                    <img src={ele}></img>
+                  <Col key={ele.url} style={{ width: 150, overflow: "hidden" }}>
+                    <Zoom>
+                      <img
+                        className="img-thumbnail"
+                        style={{ height: 150, width: 150, objectFit: "cover" }}
+                        src={getImageUrl(ele.url)}
+                      ></img>
+                    </Zoom>
                   </Col>
                 ))}
               </Row>
